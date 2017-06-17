9168a8643e13acd4fa4c046d5bffb879b211d62b
hyderit <hayder@wayne.edu>
2017-06-17 11:59:59 -0400

    removed phone

--- captcha/x5captcha.php
+++ captcha/x5captcha.php
@@ -1,7 +1,7 @@
 <?php
 include("../res/x5engine.php");
-$nameList = array("6a5","r5l","eze","lw6","gnx","t2t","7l6","4ls","548","dxk");
-$charList = array("F","W","J","6","D","D","C","X","3","V");
+$nameList = array("rvz","f82","re5","etd","3zj","myc","777","3gk","fxk","e27");
+$charList = array("J","F","3","V","5","4","3","V","2","N");
 $cpt = new X5Captcha($nameList, $charList);
 //Check Captcha
 if ($_GET["action"] == "check")
